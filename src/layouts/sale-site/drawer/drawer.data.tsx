893346156fb8b7bf603a80261ea65eb127a32fd2
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2024-08-02 23:07:48 +0500

    drawer

--- src/layouts/sale-site/drawer/drawer.data.tsx
+++ src/layouts/sale-site/drawer/drawer.data.tsx
@@ -14,14 +14,14 @@ export const saleSiteDrawerArray: NavItem[] = [
 
 export const mainStyles = (href: string, pathName: string, theme: Theme) => {
   return {
-    background: `${pathName === href ? theme.palette.gradients.primary : null}`,
+    background: `${pathName === href ? theme.palette.primary.main : null}`,
     color: `${pathName === href ? "grey.50" : "text.body"}`,
     fontWeight: pathName === href ? 700 : 400,
-    padding: "10px 16px",
+    padding: "10px 16px 10px 30px",
     fontSize: pxToRem(16),
-    borderRadius: "6px 200px 200px 6px",
+    borderRadius: "200px 200px 200px 200px",
     "&:hover": {
-      background: theme.palette.gradients.primary,
+      background: theme.palette.primary.main,
       color: "grey.50",
     },
   };
