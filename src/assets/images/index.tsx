2fb5360066e880f81ce0da42102c242e46a4f887
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2024-09-04 22:26:51 +0500

    login bg image

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -81,3 +81,4 @@ export { default as TeamMemberThirtyFive } from "./sale-site/team/member-thirty-
 export { default as TeamMemberThirtySix } from "./sale-site/team/member-thirty-six.png";
 export { default as TeamMemberThirtySeven } from "./sale-site/team/member-thirty-seven.png";
 export { default as TeamMemberThirtyEight } from "./sale-site/team/member-thirty-eight.png";
+export { default as SignInBgImg } from "./auth/sign-in-bg-img.png";
--- src/sections/auth/sign-in/index.tsx
+++ src/sections/auth/sign-in/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { SignInBgImg } from "@/assets/images";
 import { FormProvider, RHFTextField } from "@/components/react-hook-form";
-import { AUTH } from "@/constants/routes";
+import { AUTH, SALE_SITE } from "@/constants/routes";
+import { pxToRem } from "@/utils/get-font-value";
 import { Box, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -17,7 +19,52 @@ export default function SignIn() {
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
+      position={"relative"}
+      overflow={"auto"}
     >
+      <Box
+        position={"absolute"}
+        top={"5%"}
+        display={"flex"}
+        justifyContent={"center"}
+        zIndex={1}
+      >
+        <Link
+          href={SALE_SITE.HOME}
+          style={{ display: "flex", alignItems: "baseline" }}
+        >
+          <Typography
+            variant={"logo"}
+            fontSize={pxToRem(36)}
+            component={"h1"}
+            color={"text.heading"}
+          >
+            Shares
+          </Typography>
+          <Box
+            width={7}
+            height={7}
+            bgcolor={"primary.main"}
+            borderRadius={"50%"}
+          />
+        </Link>
+      </Box>
+      <Box
+        component={"span"}
+        bgcolor={"common.bgLight"}
+        position={"absolute"}
+        top={0}
+        left={0}
+        zIndex={0}
+        width={"100%"}
+        height={"50vh"}
+        sx={{
+          backgroundImage: `url(${SignInBgImg.src})`,
+          backgroundSize: "auto",
+          backgroundPosition: "center top",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
       <Box
         maxWidth={"sm"}
         width={"100%"}
@@ -25,6 +72,8 @@ export default function SignIn() {
         px={2.4}
         py={3.2}
         borderRadius={3}
+        zIndex={1}
+        mx={2}
       >
         <Grid container spacing={3.2}>
           <Grid item xs={12}>
