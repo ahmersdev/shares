a150b8f2d65842ec62b50289fdcbde7b2901ba9a
Muhammad Ahmer Akram <mahmerakram6@gmail.com>
2024-10-02 21:36:16 +0500

    forgot password

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import { ForgotPassword } from "@/sections/auth";
+
+export default function ForgotPasswordPage() {
+  return <ForgotPassword />;
+}
--- src/sections/auth/index.tsx
+++ src/sections/auth/index.tsx
@@ -2,3 +2,4 @@ export { default as SignIn } from "./sign-in";
 export { default as SignUp } from "./sign-up";
 export { default as EmailOtp } from "./email-otp";
 export { default as CreatePassword } from "./create-password";
+export { default as ForgotPassword } from "./forgot-password";
