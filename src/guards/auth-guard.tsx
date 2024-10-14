9d59e9ff83be3f939555f850a5fa41ac60382ae4
Muhammad Ahmer Akram <mahmerakram6@gmail.com>
2024-10-14 19:28:54 +0500

    guard update

--- src/guards/auth-guard.tsx
+++ src/guards/auth-guard.tsx
@@ -7,11 +7,17 @@ import { errorSnackbar } from "@/utils/api";
 import { IChildrenProps } from "@/interfaces";
 import Cookies from "js-cookie";
 import Loading from "@/app/loading";
+import { useGetUserDetailsQuery } from "@/services/web-app/settings";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 
 export default function AuthGuard({ children }: IChildrenProps) {
   const router = useRouter();
   const pathname = usePathname();
   const [isLoading, setIsLoading] = useState(true);
+
+  const { isError, error } = useGetUserDetailsQuery(null, {
+    refetchOnMountOrArgChange: true,
+  });
 
   const checkAuth = useCallback(() => {
     const tokenCookies = Cookies.get("authenticationToken");
@@ -20,8 +26,23 @@ export default function AuthGuard({ children }: IChildrenProps) {
       router.push(AUTH.SIGN_IN);
       return false;
     }
+
+    if (isError && error && "status" in error) {
+      const fetchError = error as FetchBaseQueryError;
+
+      if (
+        fetchError.status === 401 &&
+        "data" in fetchError &&
+        (fetchError.data as any).msg === "Token expired"
+      ) {
+        Cookies.remove("authenticationToken");
+        errorSnackbar("Session Expired! Login to Continue");
+        router.push(AUTH.SIGN_IN);
+        return false;
+      }
+    }
     return true;
-  }, [router]);
+  }, [router, isError, error]);
 
   useEffect(() => {
     setIsLoading(true);
