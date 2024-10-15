b1e2f3940188ac06cdc2b866e43ed00741a56c28
Muhammad Ahmer Akram <mahmerakram6@gmail.com>
2024-10-15 22:07:01 +0500

    account information page

---
+++ src/sections/web-app/client-profile/account-information/account-information.data.tsx
@@ -0,0 +1,6 @@
+export const getAccountInformationData = () => ({
+  Name: "Frontend App",
+  Email: "email@test.com",
+  "Phone Number": "+923154806474",
+  "Investor Type": "Retail",
+});
--- src/sections/web-app/client-profile/account-information/index.tsx
+++ src/sections/web-app/client-profile/account-information/index.tsx
@@ -1,16 +1,54 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { getAccountInformationData } from "./account-information.data";
 
 export default function AccountInformation() {
+  const accountInformationData = getAccountInformationData();
+
   return (
-    <Box
-      bgcolor={"grey.50"}
-      border={1}
-      borderColor={"text.stroke"}
-      borderRadius={3}
-      px={3.2}
-      py={2.4}
-    ></Box>
+    <Box maxWidth={"sm"} width={"100%"} textAlign={"center"}>
+      <Box
+        bgcolor={"grey.50"}
+        border={1}
+        borderColor={"text.stroke"}
+        borderRadius={3}
+        px={3.2}
+        py={2.4}
+      >
+        {Object?.entries(accountInformationData)?.map(([key, value], index) => (
+          <Box
+            key={key}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            mb={
+              index === Object.entries(accountInformationData).length - 1
+                ? 0
+                : 2.4
+            }
+          >
+            <Typography variant={"body3"}>{key}</Typography>
+            <Typography
+              variant={"body3"}
+              fontWeight={700}
+              color={"text.heading"}
+            >
+              {value}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+
+      <Button
+        variant={"text"}
+        size={"small"}
+        color={"error"}
+        sx={{ mt: 1 }}
+        disableElevation
+      >
+        Delete Account
+      </Button>
+    </Box>
   );
 }
