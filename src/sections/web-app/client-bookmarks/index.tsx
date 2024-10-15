bdf794b9b59c9399f9600c99305d3cf3415a92c5
Muhammad Ahmer Akram <mahmerakram6@gmail.com>
2024-10-15 22:09:36 +0500

    bookmarks page

--- src/sections/web-app/client-bookmarks/index.tsx
+++ src/sections/web-app/client-bookmarks/index.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { Grid } from "@mui/material";
+import { bookmarkedPropertiesData } from "./client-bookmarks.data";
+import { IProperty } from "@/interfaces/properties";
+import PropertiesCard from "@/components/properties-card";
+
 export default function ClientBookmarks() {
-  return <>ClientBookmarks</>;
+  return (
+    <Grid container spacing={2.4}>
+      {bookmarkedPropertiesData.map((property: IProperty) => (
+        <Grid item xs={12} sm={6} md={4} key={property.id}>
+          <PropertiesCard property={property} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
